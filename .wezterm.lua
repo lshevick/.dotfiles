f6de5b86eb693954bb3630af6d77b887cbe63d98
levi shevick <levi@oorejewelry.com>
2026-07-27 15:14:16 -0400

    adds tmux

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,8 +3,8 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.enable_tab_bar = false
-config.font_size = 20.0
---config.font = wezterm.font 'Fira Code'
+config.font_size = 16.0
+config.font = wezterm.font 'FiraCode Nerd Font'
 
 
 return config
